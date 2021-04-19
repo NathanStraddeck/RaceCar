054e807f195668b5c2fdd2adb03c9a5449169526
Noah <potato.duck.face@gmail.com>
2021-04-19 12:47:03 -0600

    Basic Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class PlayerController: MonoBehaviour
 {
+    InputHandler input;
+
     private float moveInput;
     private float turnInput;
+    private bool isCarGrounded;
+
+    public float airDrag;
+    public float groundDrag;
 
     public float fwdSpeed;
     public float revSpeed;
     public float turnSpeed;
+    public LayerMask groundLayer;
 
     public Rigidbody sphereRB;
 
@@ -18,23 +25,46 @@ public class PlayerController: MonoBehaviour
     void Start()
     {
         sphereRB.transform.parent = null;
-
+        input = InputHandler.instance;
     }
 
     void Update()
     {
-        moveInput = Input.GetAxisRaw("Vertical");
-        turnInput - Input.GetAxisRaw("Horizontal");
+        moveInput = input.move.y;
+        turnInput = input.move.x;
+
         moveInput *= fwdSpeed > 0 ? fwdSpeed : revSpeed;
 
         transform.position = sphereRB.transform.position;
 
+        float newRotation = turnInput * turnSpeed * Time.deltaTime * input.move.y;
+        transform.Rotate(0, newRotation, 0, Space.World);
 
+        RaycastHit hit;
+        isCarGrounded = Physics.Raycast(transform.position, -transform.up, out hit, 1f, groundLayer);
 
+        transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+
+        if (isCarGrounded)
+        {
+            sphereRB.drag = groundDrag;
+        }
+        else
+        {
+            sphereRB.drag = airDrag;
+        }
     }
     private void FixedUpdate()
     {
-        sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
+       
+        if (isCarGrounded)
+        {
+            sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
+        }
+        else
+        {
+            sphereRB.AddForce(transform.up * -40f);
+        }
     }
 
 }
